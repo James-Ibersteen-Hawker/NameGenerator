dacae2bf43353b7f54c4d17e393d8c84313cb948
James-Ibersteen-Hawker <526rserbinenko@frhsd.com>
2024-12-10 09:52:30 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// let entry = {
+//   first: null,
+//   last: null,
+//   entry1: null,
+//   entry2: null,
+//   entry3: null,
+// };
+
+class entry {
+  firstName;
+  lastName;
+  input1;
+  input2;
+  input3;
+  constructor(firstName, lastName, input1, input2, input3) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.input1 = input1;
+    this.input2 = input2;
+    this.input3 = input3;
+  }
+  get newFirstName() {}
+  get newMiddleName() {}
+  get newLastName() {}
+}
