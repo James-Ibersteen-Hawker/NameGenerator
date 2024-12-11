6146f288dccfa7bbd27de634bdeb5404c2b02d58
James-Ibersteen-Hawker <526rserbinenko@frhsd.com>
2024-12-11 11:50:08 -0500

    wrote the js, still some more to do with name manipulation

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-function submit() {}
-class entry {
-  firstName;
-  lastName;
-  fColor;
-  fNum;
-  input3;
-  constructor(firstName, lastName, fColor, fNum, input3) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.fColor = fColor;
-    this.fNum = fNum;
-    this.input3 = input3;
-  }
-  get newFirstName() {}
-  get newMiddleName() {}
-  get newLastName() {}
-  construct() {}
-}
+"use strict";
 const consonants = [
   "b",
   "c",
@@ -40,4 +22,73 @@ const consonants = [
   "z",
 ];
 const vowels = ["a", "e", "i", "o", "u"];
-//keep js as short as humanly possible
+function submit() {
+  let invalid = false;
+  let values = [
+    document.getElementById("fName"),
+    document.getElementById("lName"),
+    document.getElementById("fColor"),
+    document.getElementById("superPowerDrop"),
+    document.getElementById("fNum"),
+  ];
+  for (let i = 0; i < values.length; i++) {
+    if (values[i].value.search(/[a-zA-Z]/) == -1) {
+      values[i].classList.add("invalid");
+      invalid = true;
+      setTimeout(
+        () => {
+          values[i].classList.remove("invalid");
+          values[i] = undefined;
+          return;
+        },
+        500,
+        values[i]
+      );
+    } else {
+      let temp = values[i].value.trim();
+      i != 3 ? (values[i].value = "") : values[i];
+      values[i] = temp;
+    }
+  }
+  if (invalid != true) {
+    console.log(
+      new entry(
+        values[0],
+        values[1],
+        values[2],
+        values[3],
+        values[4]
+      ).construct()
+    );
+  }
+} //39 lines
+class entry {
+  firstName;
+  lastName;
+  fColor;
+  fNum;
+  input3;
+  constructor(firstName, lastName, fColor, fNum, input3) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.fColor = fColor;
+    this.fNum = fNum;
+    this.input3 = input3;
+  }
+  get newFirstName() {
+    if (this.firstName.length >= 4) return "King";
+    else return "Master";
+  }
+  get newMiddleName() {
+    return "val";
+  }
+  get newLastName() {
+    return "val";
+  }
+  construct() {
+    let firstName = this.newFirstName;
+    let middleName = this.newMiddleName;
+    let lastName = this.newLastName;
+    return `${firstName} ${middleName} ${lastName}`;
+  }
+}
